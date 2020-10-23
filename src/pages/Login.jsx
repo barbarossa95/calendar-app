a7caef5a2ac99fdcfd6ffaa81c9363d44262389e
Greshilov Vladimir <greshilov@a2design.biz>
2020-10-23 18:15:28 +0600

    updated: login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
 
 import { login } from '../store/actions/user';
+import { go } from '../store/actions/navigation';
+
+import { getLoginMessage } from '../store/selectors';
 
 import LoginForm from '../components/LoginForm';
 
-const mapStateToProps = () => ({
+const mapStateToProps = (state) => ({
+    message: getLoginMessage(state)
 });
 
-const mapDispatchToProps = { login };
+const mapDispatchToProps = { login, go };
 
 export default connect(
   mapStateToProps,
