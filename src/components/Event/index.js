bcaeab50429c89384afa1839657d20e9cc0d3e7e
Greshilov Vladimir <greshilov@a2design.biz>
2020-10-24 02:33:48 +0600

    added event component

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Event = () => {
-  return <section>Event</section>;
+import './Event.scss';
+
+const Event = ({ event }) => {
+  const { _id, title, start, duration } = event,
+    style = {
+      height: `${(duration / 5) * 50}px`,
+    };
+
+  return (
+    <div key={_id} className="event" style={style}>
+      <h1 className="event__title">{title}</h1>
+    </div>
+  );
 };
 
 export default Event;
