ff3c2b8ac3d354f61214c8d8ce1a825fcb264fd9
Greshilov Vladimir <greshilov@a2design.biz>
2020-10-23 12:09:50 +0600

    updated: index js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { store, history } from './store';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <Provider store={store}>
+    <App history={history} />
+  </Provider>,
   document.getElementById('root')
 );
 
