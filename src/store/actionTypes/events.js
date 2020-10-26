e1204a7e08efae855a775e4c09f6105e7af34386
Greshilov Vladimir <greshilov@a2design.biz>
2020-10-26 11:20:44 +0600

    fixed typo

--- src/store/actionTypes/events.js
+++ src/store/actionTypes/events.js
@@ -1,4 +1,6 @@
 export const ADD_EVENT = 'ADD_EVENT';
 export const FETCH_EVENTS = 'FETCH_EVENTS';
 
-export const CLEAR_EVENTS = 'FETCH_EVENTS';
+export const CLEAR_EVENTS = 'CLEAR_EVENTS';
+
+export const EDIT_EVENT = 'EDIT_EVENT';
