ffa629436a24a760b4a84e38c9a084f02a4eada0
Greshilov Vladimir <greshilov@a2design.biz>
2020-10-27 14:43:09 +0600

    added routes for edit and add event

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,16 @@ import { Route, Switch } from 'react-router';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register.jsx';
+import Add from '../pages/Add.jsx';
+import Edit from '../pages/Edit.jsx';
 
 const routes = (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/login" component={Login} />
     <Route path="/register" component={Register} />
+    <Route path="/add" component={Add} />
+    <Route path="/edit" component={Edit} />
     <Route
       render={() => {
         return (
