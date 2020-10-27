fa89048a1d86d993b567c71c1b52f4faec402259
Greshilov Vladimir <greshilov@a2design.biz>
2020-10-27 14:42:10 +0600

    url selector for event

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -102,3 +102,17 @@ export const getEventsGrouped = createSelector([getEvents], (events) => {
     return result;
   }, []);
 });
+
+export const getEventFromRoute = (state) => {
+  function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  }
+  const id = getParameterByName('id');
+
+  return state.events.events.find((e) => e._id == id);
+};
